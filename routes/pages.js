7e5e7694f05371a25f6b5f432c59ece1178291cf
Nathan Patton <npatton1206@gmail.com>
2020-09-10 16:28:00 -0400

    Movie play partially there

--- routes/pages.js
+++ routes/pages.js
@@ -13,7 +13,17 @@ const imageBaseUrl = process.env.IMAGE_BASE_URL
 const key = process.env.KEY
 const secret = process.env.SECRET_KEY
 
-router.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", 'maxcdn.bootstrapcdn.com', 'ajax.googleapis.com'], styleSrc: ["'self'", 'fonts.googleapis.com'], imgSrc: ["'self'", 'image.tmdb.org'] } }))
+router.use(helmet.contentSecurityPolicy({ directives: {
+    defaultSrc: ["'self'"],
+    styleSrc: ["'self'", "https:", "'unsafe-inline'"],
+    fontSrc: ["'self'", "https:", "data:"],
+    imgSrc: ["'self'", "https://image.tmdb.org"],
+    scriptSrc: ["'self'", "'unsafe-inline'"],
+    blockAllMixedContent: [],
+    upgradeInsecureRequests: [],
+    baseUri: ["'self'"],
+    frameAncestors: ["'self'"],
+  }}))
 
 // router.use((req, res, next) => {
 //     res.locals.dom = dom
@@ -55,7 +65,7 @@ router.get('/movie/:id', (req, res, next) => {
     request.get(movieUrl, (error, response, movieData) => {
         const parsedData = JSON.parse(movieData)
         res.render('movie', {
-            parsedData
+            parsedData, movieID
         })
     })
 })
@@ -63,17 +73,15 @@ router.get('/movie/:id', (req, res, next) => {
 router.get('/play/:id', (req, res, next) => {
     const movieID = req.params.id
 
-    request.get('https://json.geoiplookup.io/api', (error, response, ipData) => {
-        const data = JSON.parse(ipData)
-        console.log(data, 68)
-        const ipAdd = data.ip
-        const playMovie = `https://vsrequest.video/request.php?key=${key}&secret_key=${secret}&video_id=${movieID}&tmdb=1&tv=0&s=*0&ip=${ipAdd}`
+    request.get('http://ip6only.me/api/', (error, response, ipData) => {
+        const data = ipData.substring(5,42)
+        const playMovie = `https://streamvideo.link/getvideo?key=${key}&video_id=${movieID}&tmdb=1`        
+        // const playMovie = `https://vsrequest.video/request.php?key=${key}&secret_key=${secret}&video_id=${movieID}&tmdb=1&tv=0&s=*0&ip=${data}`
+        console.log(playMovie)
         request.get(playMovie, (error, response, movieAddress) => {
-            console.log(movieAddress, 72)
-            const parsedData = JSON.parse(movieAddress)
-            console.log(parsedData, 74)
+            console.log(movieAddress)
             res.render('play', {
-                playMovie: parsedData
+                movieAddress: movieAddress
             })
         })
     })
