fd52dd66f3934a0351d0dbcf93dfb6f2d09dca66
Nathan Patton <npatton1206@gmail.com>
2020-09-18 13:10:58 -0400

    Remove unnecessary files

--- models/genreList.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const genre = mongoose.Schema({
-    genreName: String
-})
-
-const genreList = mongoose.model('genreList', genre)
-
-module.exports = genreList
