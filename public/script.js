c7a3898fec586f9494c8227cd165990fd35adec8
Nathan Patton <npatton1206@gmail.com>
2020-09-19 21:05:53 -0400

    Update script

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   path: '/peerjs',
-  host: '/',
+  host: 'flixify.herokuapp.com/',
   port: '4000'
 })
 let myVideoStream;
