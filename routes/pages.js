840938a913b9b1bea6f289dcad7cc929684b5c3f
Nathan Patton <npatton1206@gmail.com>
2020-09-19 20:56:58 -0400

    CSP

--- routes/pages.js
+++ routes/pages.js
@@ -36,7 +36,7 @@ app.use(helmet.contentSecurityPolicy({
             "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
             "https://kit.fontawesome.com/c939d0e917.js",
         ],
-        connectSrc: ["'self'", "wss://" + "flixify.herokuapp.com:4000", "https://flixify.herokuapp.com:4000/peerjs/peerjs/id?ts="],
+        connectSrc: ["'self'", "wss://" + "flixify.herokuapp.com:4000", "flixify.herokuapp.com:4000"],
         blockAllMixedContent: [],
         upgradeInsecureRequests: [],
         baseUri: ["'self'"],
