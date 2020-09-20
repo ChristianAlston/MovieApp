469ae3ce507d8e9143ec9f930d2fecd38a296471
Nathan Patton <npatton1206@gmail.com>
2020-09-19 20:02:06 -0400

    Deployment fix

--- routes/pages.js
+++ routes/pages.js
@@ -33,7 +33,7 @@ app.use(helmet.contentSecurityPolicy({
             "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
             "https://kit.fontawesome.com/c939d0e917.js",
         ],
-        connectSrc: ["'self'", "ws://https://flixify.herokuapp.com"],
+        connectSrc: ["'self'", "ws://https://flixify.herokuapp.com:4000"],
         blockAllMixedContent: [],
         upgradeInsecureRequests: [],
         baseUri: ["'self'"],
